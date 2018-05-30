2bc2633afa258a86dbf2cb2ce6b82446045d7f7d
Sahil Gulati <sahilgulati@paisabazaar.com>
2018-05-30 14:41:31 +0530

    Tested new functionality

--- test.js
+++ test.js
@@ -6,3 +6,4 @@ console.log(Noopath.getConfig().var.www.node.noopath.modules.FileMisc)
 console.log(Noopath.getFromConfig("var.www.node"))
 console.log(Noopath.load("index"));
 console.log(Noopath.loadByFilter("config","git\/config$"));
+console.log(Noopath.getAll("config"));
