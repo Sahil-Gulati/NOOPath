50045aca3701d6254790191fb154622161670013
Sahil Gulati <sahilgulati@paisabazaar.com>
2018-07-23 15:01:32 +0530

    Removed testing procedure.

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var Noopath = require("./Noopath")
-Noopath.setPath("/var/www/node/noopath")
+var Noopath = require("./Noopath");
+Noopath.setPath("/var/www/node/noopath");
 Noopath.setIgnorePaths(["/var/www/node/noopath/nbproject/"])
-Noopath.setIgnoreExtensions(["json"])
-console.log(Noopath.getConfig().var.www.node.noopath.modules.FileMisc)
-console.log(Noopath.getFromConfig("var.www.node"))
-console.log(Noopath.load("index"));
-console.log(Noopath.loadByFilter("config","git\/config$"));
-console.log(Noopath.getAll("config"));
+Noopath.setIgnoreExtensions(["json"]);
+Noopath.debug(true);
+Noopath.load("index");
+Noopath.load("config");
+Noopath.load("maker");
+Noopath.getFromConfig("var.www.node.noopath.modules.maker");
+Noopath.loadByFilter("config","modules");
