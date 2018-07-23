d33c9a87f4fff97b56202c80182a220f85c0d9ac
Sahil Gulati <sahilgulati@paisabazaar.com>
2018-07-23 16:23:36 +0530

    Changed indentation and added new function call.

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 var Noopath = require("./Noopath");
+
 Noopath.setPath("/var/www/node/noopath");
 Noopath.setIgnorePaths(["/var/www/node/noopath/nbproject/"])
 Noopath.setIgnoreExtensions(["json"]);
-Noopath.debug(true);
-Noopath.load("index");
-Noopath.load("config");
-Noopath.load("maker");
-Noopath.getFromConfig("var.www.node.noopath.modules.maker");
-Noopath.loadByFilter("config","modules");
+
+Noopath.debug(false);
+
+
+console.log("Noopath.load\t\t",Noopath.load("config"));
+console.log("Noopath.loadOrElse\t",Noopath.loadOrElse("x.json","y.json","maker"));
+console.log("Noopath.loadByFilter\t",Noopath.loadByFilter("config","modules"));
+console.log("Noopath.loadFromFolder\t",Noopath.loadFromFolder("noopath","Misc"));
+
+
+console.log("Noopath.getFromConfig\t",Noopath.getFromConfig("var.www.node.noopath.modules.maker"));
+console.log("Noopath.getAll\t\t",Noopath.getAll("config"));
+console.log("Noopath.getConfig\t",Noopath.getConfig());
